be59e7e21e505aa3dfb9bc8db815a22f7e235e50
Wozniak Kinga <wozniak@login2.baobab>
2023-12-04 18:38:11 +0100

    adding structure and template conversion script

---
+++ src/string_constants.py
@@ -0,0 +1,7 @@
+
+# Directory containing the ROOT files
+root_dir = "data/jetclass/train_10M"
+output_dir = "data/jetclass/ntuples"
+output_file = "combined_data.h5"
+
+dir_h5_mc16 = '/eos/user/m/malgren/ftag_data/'
