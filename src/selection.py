fixing invariant mass computation

src/selection.py
@@ -1,27 +1,31 @@
 import numpy as np
 import awkward as awk
 
-def calc_invariant_mass(pt_t:awk.highlevel.Array, phi:awk.highlevel.Array, eta:awk.highlevel.Array, part_m:float) -> awk.highlevel.Array:
+def calc_invariant_mass(pt:awk.highlevel.Array, phi:awk.highlevel.Array, eta:awk.highlevel.Array, part_m:float) -> awk.highlevel.Array:
 
-	px = pt_t * np.cos(phi)
-	py = pt_t * np.sin(phi)
-	pz = pt_t * np.sinh(eta)
-	pt3d = np.sqrt(px**2 + py**2 + pz**2)
-	e3d = np.sqrt((pt_t*np.cosh(eta))**2 + part_m**2)
+	# compute x,y,z momentum of each lepton and sum -> px,py,pz of dilepton system
+	px = ak.sum(pt * np.cos(phi),axis=1)
+	py = ak.sum(pt * np.sin(phi),axis=1)
+	pz = ak.sum(pt * np.sinh(eta),axis=1)
+
+	moment2 = px**2 + py**2 + pz**2
+	energy = ak.sum(np.sqrt((pt*np.cosh(eta))**2 + part_m**2),axis=1)
+
+	return np.sqrt(energy**2 - moment2)
 	
-	return awk.sum(np.sqrt(e3d**2 - pt3d**2), axis=1)
-
 
 def select(samples:awk.highlevel.Array) -> awk.highlevel.Array:
 
 	# z mass window
-	z_m_min, z_m_max = 80., 100.
+	z_m_min, z_m_max = 80e3, 100e3.
+	# particle masses
+	ele_m, mu_m = 511e-3, 105.7
 
 	# compute invariant mass of electrons
-	samples['ee_m'] = calc_invariant_mass(samples.el_pt, samples.el_phi, samples.el_eta, part_m=511e-3)
+	samples['ee_m'] = calc_invariant_mass(samples.el_pt, samples.el_phi, samples.el_eta, part_m=ele_m)
 
 	# compute invariant mass of muons
-	samples['mumu_m'] = calc_invariant_mass(samples.mu_pt, samples.mu_phi, samples.mu_eta, part_m=105.7)
+	samples['mumu_m'] = calc_invariant_mass(samples.mu_pt, samples.mu_phi, samples.mu_eta, part_m=mu_m)
 
 	# invariant mass of electrons 80-100 GeV
 	mask_ee_m = samples['ee_m'] > z_m_min & samples['ee_m'] < z_m_max