d2c0cb9eb3c298a6ff3bf11e4fa342c9fb9af5e0
kingusiu <kinga.wozniak@gmail.com>
2024-04-08 22:29:53 +0200

    fixing path constants (mc and data root dirs), updating/fixing mc vs data script

--- scripts/mc_vs_data_plots.py
+++ scripts/mc_vs_data_plots.py
@@ -13,36 +13,69 @@ import src.string_constants as stco
 import src.reader as read
 import src.selection as sele
 
+if __name__ == '__main__':
 
+	feature_names = ['el_e','mu_e','el_charge','mu_charge','el_pt','mu_pt', 'el_phi','mu_phi',\
+                      'el_eta', 'mu_eta', 'jet_pt', 'jet_e', 'jet_GN2_pu', 'jet_GN2_pb', 'jet_GN2_pc']
 
-# read MC (signal and background)
-# signal (one file per ee, mumu, tautau)
-# read b-tagged electron samples
-	path_ee = glob.glob(os.path.join(stco.generator_in_dir_lightjet,'*'+stco.ds_ids['ee'][0]+'*'))[0]
+	### read MC (signal and background)
+
+	## signal (one file per ee, mumu, tautau)
+	
+	# read electron samples
+	path_ee = glob.glob(os.path.join(stco.in_dir_mc,'*'+stco.ds_ids_sig['ee'][0]+'*'))[0]
 	fname_ee = os.listdir(path_ee)[3]
 	samples_ee = read.read_samples_from_file(os.path.join(path_ee,fname_ee),feature_names)
 
-	# select electron samples
-	samples_selected_ee = sele.select_lightjets(samples_ee)
-
 	# read muon samples
-	path_mumu = glob.glob(os.path.join(stco.generator_in_dir_lightjet,'*'+stco.ds_ids['mumu'][0]+'*'))[0]
+	path_mumu = glob.glob(os.path.join(stco.in_dir_mc,'*'+stco.ds_ids_sig['mumu'][0]+'*'))[0]
 	fname_mumu = os.listdir(path_mumu)[3]
 	samples_mumu = read.read_samples_from_file(os.path.join(path_mumu,fname_mumu),feature_names)
 
-	# select muon samples
-	samples_selected_mumu = sele.select_lightjets(samples_mumu)
+	# read tau samples
+	path_tautau = glob.glob(os.path.join(stco.in_dir_mc,'*'+stco.ds_ids_sig['tautau'][0]+'*'))[0]
+	fname_tautau = os.listdir(path_tautau)[3]
+	samples_tautau = read.read_samples_from_file(os.path.join(path_tautau,fname_tautau),feature_names)
 
 
+	samples_sig = awk.concatenate([samples_ee,samples_mumu,samples_tautau])
 
-# read data
+	# z+jets samples that passed lightjet selection (contains z+light jets, z+b-jets, z+c-jets)
+	samples_zplusjets = sele.select_lightjets(samples_sig)
+
+	## background
+
+	# ttbar
+	path_ttbar = glob.glob(os.path.join(stco.in_dir_mc,'*'+stco.ds_ids_bg['ttbar'][0]+'*'))[0]
+	fname_ttbar = os.listdir(path_ttbar)[3]
+	samples_ttbar = read.read_samples_from_file(os.path.join(path_ttbar,fname_ttbar),feature_names)
+	
+	# zqqzll
+	path_zqqzll = glob.glob(os.path.join(stco.in_dir_mc,'*'+stco.ds_ids_bg['zqqzll'][0]+'*'))[0]
+	fname_zqqzll = os.listdir(path_zqqzll)[3]
+	samples_zqqzll = read.read_samples_from_file(os.path.join(path_zqqzll,fname_zqqzll),feature_names)
+
+	# wqqzll
+	path_wqqzll = glob.glob(os.path.join(stco.in_dir_mc,'*'+stco.ds_ids_bg['wqqzll'][0]+'*'))[0]
+	fname_wqqzll = os.listdir(path_wqqzll)[3]
+	samples_wqqzll = read.read_samples_from_file(os.path.join(path_wqqzll,fname_wqqzll),feature_names)
+
+	# ggZllZqq
+	path_ggZllZqq = glob.glob(os.path.join(stco.in_dir_mc,'*'+stco.ds_ids_bg['ggZllZqq'][0]+'*'))[0]
+	fname_ggZllZqq = os.listdir(path_ggZllZqq)[3]
+	samples_ggZllZqq = read.read_samples_from_file(os.path.join(path_ggZllZqq,fname_ggZllZqq),feature_names)
 
 
-# pass MC through light jet selection
+	samples_zz = awk.concatenate([samples_zqqzll,samples_ggZllZqq])
+
+	# diboson and ttbar samples that passed lightjet selection (contains zw, zz and ttbar)
+	samples_ttbar = sele.select_lightjets(samples_ttbar)
+	samples_zz = sele.select_lightjets(samples_zz)
+	samples_zw = sele.select_lightjets(samples_wqqzll)
+
+	# split signal samples into z+lightjet, z+bjet and z+cjet
 
 
-# pass data through light jet selection
+	# plot as stacked histogram
 
-
-# plot as stacked histogram
 
--- src/string_constants.py
+++ src/string_constants.py
@@ -19,20 +19,12 @@ import os
 # ************************* INPUTS ************************** #
 
 # root input directory
-in_dir_root = '/eos/atlas/atlascerngroupdisk/perf-flavtag/calib/negtag/Rel24_GN2v01/mc20_Run2/Ntuples_Nominal/'
+in_dir_root = '/eos/atlas/atlascerngroupdisk/perf-flavtag/calib/negtag/Rel24_GN2v01/Ntuples_Nominal/'
 
 # input directory for run 2 mc 2017 Period D (signal and background samples)
-in_dir_mc_run2_D = os.path.join(in_dir_root,'PeriodD')
+in_dir_mc = os.path.join(in_dir_root,'mc20d')
 
-# atm only period D
-dir_dict_mc_run2 = {
-    
-        'sig_mg' : os.path.join(in_dir_mc_run2_D,'MG'), # signal samples by MG 
-        'sig_sh' : os.path.join(in_dir_mc_run2_D,'SH'), # signal samples by Sherpa
-        'bg' : os.path.join(in_dir_mc_run2_D,'Other'), # background samples: Sherpa diboson and PhPy ttbar dilepton 
-
-}
-
+in_dir_data = '/eos/atlas/atlascerngroupdisk/perf-flavtag/calib/negtag/Rel24_GN2v01/Ntuples_Nominal/dataRun2_daod_ftag2/'
 
 # ************************* OUTPUTS ************************** #
 
@@ -52,16 +44,16 @@ results_fig_dir = 'results/fig'
 # *********************************************************** #
 
 # particle / process dataset IDS
-sig_ds_ids = {
+ds_ids_sig = {
     'ee' : ['506193', '506194', '506195'],
     'mumu' : ['506196', '506197', '506198'],
     'tautau': ['512198', '512199', '512200']
 }
 
-bg_ds_ids = {
+ds_ids_bg = {
     'ttbar' : 410472,
     'zqqzll' : 363356,
-    '363358' : 363358,
-    '364302' : 364302,
+    'wqqzll' : 363358,
+    'ggZllZqq' : 364302,
 }
 
